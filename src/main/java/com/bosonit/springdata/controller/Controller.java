3ae81351e5bd7b08ec3e6cccdd7ecdcf8089a630
Emar Martinez <emar.martinez@bosonit.com>
2022-10-11 16:08:46 +0200

    buenas practicas final

--- src/main/java/com/bosonit/springdata/controller/Controller.java
+++ src/main/java/com/bosonit/springdata/controller/Controller.java
@@ -3,9 +3,12 @@ package com.bosonit.springdata.controller;
 import com.bosonit.springdata.domain.Student;
 import com.bosonit.springdata.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.net.URI;
+
 
 @RestController
 @RequestMapping("/student")
@@ -14,29 +17,47 @@ public class Controller {
     StudentRepository studentRepository;
 
     @PostMapping
-    public Student addStudent(@RequestBody Student student) {
-        return studentRepository.save(student);
+    public ResponseEntity<Student> addStudent(@RequestBody Student student) {
+        URI location = URI.create("/student");
+        return ResponseEntity.created(location).body(studentRepository.save(student));
     }
 
     @GetMapping("/{id}")
-    public Student getStudentById(@PathVariable int id) {
-        return studentRepository.findById(id).orElseThrow();
+    public ResponseEntity<Student> getStudentById(@PathVariable int id) {
+        try {
+            return ResponseEntity.ok().body(studentRepository.findById(id).orElseThrow());
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping
-    public void deleteStudentById(@RequestParam int id) {
-        studentRepository.deleteById(id);
+    public ResponseEntity<String> deleteStudentById(@RequestParam int id) {
+        try {
+            studentRepository.findById(id).orElseThrow();
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok().body("student with id: "+id+" was deleted");
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
+    public Iterable<Student> getAllStudents(
+            @RequestParam(defaultValue = "0", required = false) int pageNumber,
+            @RequestParam(defaultValue = "4", required = false) int pageSize) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        return studentRepository.findAll(pageRequest).getContent();
     }
 
-    @PutMapping("/{id}")
-    public Student updateStudent(@RequestBody Student student,@PathVariable int id) {
-        studentRepository.findById(id).orElseThrow();
-        return studentRepository.save(student);
+    @PutMapping
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student) {
+        try {
+            studentRepository.findById(student.getId()).orElseThrow();
+            return  ResponseEntity.ok().body(studentRepository.save(student));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
--- src/main/java/com/bosonit/springdata/repository/StudentRepository.java
+++ src/main/java/com/bosonit/springdata/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.bosonit.springdata.repository;
 
 import com.bosonit.springdata.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Integer> {
+public interface StudentRepository extends JpaRepository<Student, Integer>,
+        PagingAndSortingRepository<Student, Integer> {
 }
